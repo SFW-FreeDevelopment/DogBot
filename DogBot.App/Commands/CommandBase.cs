f8e301f82a6692ba290344961fff40ec889f25a7
Steven Garberg <steven.garberg.jr@gmail.com>
2021-10-10 14:37:37 -0400

    Added basic commands

---
+++ DogBot.App/Commands/CommandBase.cs
@@ -0,0 +1,9 @@
+﻿using Discord.Commands;
+
+namespace DogBot.App.Commands
+{
+    public abstract class CommandBase : ModuleBase<SocketCommandContext>
+    {
+        protected string Mention => Context.Message.Author.Mention;
+    }
+}
